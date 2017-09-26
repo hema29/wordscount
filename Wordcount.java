fac531f117587531da972c825848fbff58036d39
hema29 <31930095+hema29@users.noreply.github.com>
2017-09-26 13:10:28 +0530

    Add files via upload

---
+++ Wordcount.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class WordCount
+{ 
+	public static void main(String args[])
+	{ 
+		String s = "welcome";
+ 
+		int count = 1;
+ 
+		for (int i = 0; i < s.length() - 1; i++)
+		{
+			if ((s.charAt(i) == ' ') && (s.charAt(i + 1) != ' '))
+			{
+				count++;
+ 
+			}
+		}
+ 		System.out.println("Number of words in a string = " + count);
+	}
+}
